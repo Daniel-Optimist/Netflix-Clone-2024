b3c3963dc0d0247c1cf924833ab8e23dbc2b4f5e
Daniel-Optimist <danielkindg@gmail.com>
2024-11-26 00:17:00 -0600

    Header and Footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
   
   return (
     <div className= "App">
-      <h1>Hello World! It is working!</h1>
+      {/* <h1>Hello World! It is working!</h1> */}
+      <Home/>
     </div>
   )
 }
--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // bek recommendation : base then put the remote rep in quotation 
+  base: "/Netlfix-Clone-2024/",
+});
+
+
+// Bek
+// export default defineConfig({ plugins: [react()], base: "/test-deploye/", });
