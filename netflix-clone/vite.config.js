9f8f90eb6ae14c9aa8c9ed1c8bc9f6c935f8842e
Daniel-Optimist <danielkindg@gmail.com>
2024-11-26 23:50:02 -0600

    corrected a typo in vite.config

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -5,9 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // bek recommendation : base then put the remote rep in quotation 
-  base: "/Netlfix-Clone-2024/",
+  base: "/Netflix-Clone-2024/",
 });
 
 
 // Bek
 // export default defineConfig({ plugins: [react()], base: "/test-deploye/", });
+
+
+// vite.config.js
+// export default {
+//   base: '/repository-name/',  // Replace with your repository's name
+//   build: {
+//     outDir: 'dist'
+//   }
+// }
